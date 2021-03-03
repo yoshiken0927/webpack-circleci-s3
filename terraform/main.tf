b49a1411437a0fbae84c5fd083e5c63d1def8776
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-03-03 19:35:34 +0900

    ばプリックアクセスの設定を修正

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,19 @@ provider "aws" {
 resource "aws_s3_bucket" "b" {
   bucket = "yoshino-tf-test-bucket"
   acl    = "private"
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.b.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 terraform {
