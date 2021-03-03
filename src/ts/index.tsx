fc6d0c57a8516c98707692fbb9e91673306868aa
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-03-03 17:48:22 +0900

    CSSを追加

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import "../css/Index.css";
 
 //コンポーネントを定義
 function Index() {
   return (
-    <p>Hello World v1.0.3</p>
+    <p className="red">Hello World v1.0.3</p>
   );
 };
 
